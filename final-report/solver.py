10f89ba1cd4512dcb1265a20976cb94d2a8d0170
thidang2901 <dkthi2901@gmail.com>
2021-07-21 04:18:56 +0700

    add type 1, 2

--- final-report/solver.py
+++ final-report/solver.py
@@ -1,7 +1,9 @@
 import numpy as np
 import constants
 import data
-import sys
+
+from sympy import Matrix, init_printing
+init_printing(use_unicode=True)
 
 
 class BaiToan(object):
@@ -19,8 +21,8 @@ class BaiToan(object):
         self.du_lieu = du_lieu
 
     def phan_tich(self, de_bai):
-        dang = "kiem_tra_co_so"
-        du_lieu = data.kiem_tra_co_so["d"]
+        dang = "kiem_tra_dltt"
+        du_lieu = data.kiem_tra_dltt["c"]
         return [dang, du_lieu]
 
     def giai(self):
@@ -29,15 +31,21 @@ class BaiToan(object):
             return
 
         if self.dang_bai_toan == constants.KIEM_TRA_THTT:
-            self.kiem_tra_thtt()
+            self.__kiem_tra_du_lieu__(["target", "given"])
+            if not self.__error:
+                self.kiem_tra_thtt()
         elif self.dang_bai_toan == constants.KIEM_TRA_DLTT:
-            self.kiem_tra_dltt()
+            self.__kiem_tra_du_lieu__(["given"])
+            if not self.__error:
+                self.kiem_tra_dltt()
         elif self.dang_bai_toan == constants.KIEM_TRA_CO_SO:
             self.__kiem_tra_du_lieu__(["given", "dimR"])
             if not self.__error:
                 self.kiem_tra_co_so()
         elif self.dang_bai_toan == constants.TIM_CO_SO:
-            self.tim_co_so()
+            self.__kiem_tra_du_lieu__(["given"])
+            if not self.__error:
+                self.tim_co_so()
         else:
             self.__buoc_giai__("Không xác định được dạng bài toán.")
             return
@@ -57,10 +65,49 @@ class BaiToan(object):
                 print(err)
 
     def kiem_tra_thtt(self):
-        return False
+        ket_qua = True
+        given = np.array(self.du_lieu["given"])
+        target = np.array([self.du_lieu["target"]])
+
+        self.__buoc_giai__(f'\nBước 1: Ma trận hóa')
+        matrix = np.append(given, target, axis=0)
+        matrix_eq = Matrix(matrix.T)
+        self.__buoc_giai__(self.print_ma_tran(matrix_eq.tolist(), prefix='\t', split_at=len(given)))
+
+        self.__buoc_giai__(f'\nBước 2: Biến đổi về ma trận bậc thang sử dụng biến đổi sơ cấp trên dòng')
+        echelon_matrix = self.ma_tran_bac_thang(matrix_eq)
+        self.__buoc_giai__(self.print_ma_tran(echelon_matrix.tolist(), prefix='\t', split_at=len(given)))
+
+        for row in range(echelon_matrix.shape[0]):
+            sum_row = sum(np.array(echelon_matrix[row, :given.shape[0]]).squeeze())
+            if sum_row == 0 and echelon_matrix[row, -1] != 0:
+                self.__buoc_giai__(f'\tHệ phương trình vô nghiệm')
+                ket_qua = False
+
+        self.__buoc_giai__(f'\nKết luận: u{"" if ket_qua else " không"} là tổ hợp tuyến tính của các vector đã cho')
+        return ket_qua
 
     def kiem_tra_dltt(self):
-        return False
+        ket_qua = True
+        given = np.array(self.du_lieu["given"])
+
+        self.__buoc_giai__(f'\nBước 1: Ma trận hóa')
+        matrix = Matrix(given)
+        self.__buoc_giai__(self.print_ma_tran(matrix.tolist(), prefix='\t', split_at=len(given)))
+
+        self.__buoc_giai__(f'\nBước 2: Biến đổi về ma trận bậc thang')
+        echelon_matrix = self.ma_tran_bac_thang(matrix)
+        self.__buoc_giai__(self.print_ma_tran(echelon_matrix.tolist(), prefix='\t', split_at=len(given)))
+
+        self.__buoc_giai__(f'\nBước 3: Xác định hạng của ma trận')
+        rank = self.tinh_hang(echelon_matrix)
+        self.__buoc_giai__(f'\trank(A) = {rank} {"=" if rank == len(given) else "<"} số vector')
+        if rank < matrix.shape[0]:
+            ket_qua = False
+
+        self.__buoc_giai__(
+            f'\nKết luận: tập vector đã cho {"độc lập tuyến tính" if ket_qua else "phụ thuộc tuyến tính"}')
+        return ket_qua
 
     def kiem_tra_co_so(self):
         ket_qua = False
@@ -69,7 +116,7 @@ class BaiToan(object):
         matranA = self.du_lieu["given"]
         dimR = self.du_lieu["dimR"]
 
-        self.__buoc_giai__(f'\nBước 1: Xây dựng ma trận từ {ten_tap_hop}')
+        self.__buoc_giai__(f'\nBước 1: Lập ma trận từ {ten_tap_hop}')
         self.__buoc_giai__(f'\tĐặt ma trận A = {matranA}')
         self.__buoc_giai__(
             f'\tTa có dimA = {len(matranA)}{f" không bằng dimR = {dimR}" if len(matranA) != dimR else ""}')
@@ -87,13 +134,24 @@ class BaiToan(object):
                 ket_qua = True
 
         self.__buoc_giai__(f'\nKết luận: {ten_tap_hop}{"" if ket_qua else " không"} là cơ sở của R{dimR}')
-
         return ket_qua
 
     def tim_co_so(self):
+        # ten_tap_hop = self.du_lieu["name"] if "name" in self.du_lieu else "tập hợp vector đã cho"
+        # matranA = self.du_lieu["given"]
+        # dimR = self.du_lieu["dimR"]
+        #
+        # self.__buoc_giai__(f'\nBước 1: Lập ma trận từ {ten_tap_hop}')
+        # self.__buoc_giai__(f'\tĐặt ma trận A = {matranA}')
+        # self.__buoc_giai__(
+        #     f'\tTa có dimA = {len(matranA)}{f" không bằng dimR = {dimR}" if len(matranA) != dimR else ""}')
+        #
+        # self.__buoc_giai__(f'\nKết luận: {ten_tap_hop}{"" if ket_qua else " không"} là cơ sở của R{dimR}')
+
         return [False, ""]
 
-    def tinh_det(self, matrix):
+    @staticmethod
+    def tinh_det(matrix):
         det = np.linalg.det(np.array(matrix))
         try:
             det = int(det)
@@ -102,6 +160,45 @@ class BaiToan(object):
         finally:
             return det
 
+    @staticmethod
+    def tinh_hang(matrix):
+        rank = matrix.shape[0]
+        for row in range(matrix.shape[0]):
+            sum_row = sum(np.array(matrix[row, :]).squeeze())
+            if sum_row == 0:
+                rank -= 1
+        return rank
+
+    def ma_tran_bac_thang(self, matrix):
+        if not matrix:
+            return
+        lead = 0
+        row_count = matrix.shape[0]
+        col_count = matrix.shape[1]
+        for r in range(row_count):
+            if lead >= col_count:
+                return matrix
+            i = r
+            while matrix[i, lead] == 0:
+                i += 1
+                if i == row_count:
+                    i = r
+                    lead += 1
+                    if col_count == lead:
+                        return matrix
+
+            matrix[i, :], matrix[r, :] = matrix[r, :], matrix[i, :]
+            # Phân tử nhân
+            lv = matrix[r, lead]
+            matrix[r, :] = matrix[r, :] / lv
+            for i in range(row_count):
+                if i != r:
+                    lv = matrix[i, lead]
+                    matrix[i, :] = matrix[i, :] - lv * matrix[r, :]
+            lead += 1
+            self.__buoc_giai__(self.print_ma_tran(matrix.tolist(), prefix='\t', split_at=3))
+        return matrix
+
     def __kiem_tra_du_lieu__(self, req_fields):
         for field in req_fields:
             if field not in self.du_lieu:
@@ -109,3 +206,13 @@ class BaiToan(object):
 
     def __buoc_giai__(self, step):
         self.__loi_giai.append(step)
+
+    @staticmethod
+    def print_ma_tran(matrix, prefix='', split_at=None):
+        matrix_str = ''
+        for row in matrix:
+            matrix_str += f'\n{prefix}'
+            matrix_str += '\t'.join([str(cell) for cell in row[:split_at]])
+            if row[split_at:]:
+                matrix_str += f'\t|\t{str(row[split_at:][0])}'
+        return matrix_str
--- final-report/test.py
+++ final-report/test.py
@@ -10,36 +10,36 @@ class TestToHopTuyenTinh(unittest.TestCase):
     def test_valid(self):
         bai_toan.nhap_du_lieu(kiem_tra_thtt["a"])
         result = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result[0], True, "Should be True")
+        self.assertEqual(result, True, "Should be True")
 
     def test_nghiem_duy_nhat(self):
         bai_toan.nhap_du_lieu(kiem_tra_thtt["b"])
         result = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result[0], True, "Should be True")
+        self.assertEqual(result, True, "Should be True")
 
     def test_vo_nghiem(self):
         bai_toan.nhap_du_lieu(kiem_tra_thtt["c"])
         result = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result[0], False, "Should be False")
+        self.assertEqual(result, False, "Should be False")
 
     def test_vo_so_nghiem(self):
         bai_toan.nhap_du_lieu(kiem_tra_thtt["d"])
         result = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result[0], False, "Should be False")
+        self.assertEqual(result, True, "Should be True")
 
     def test_doc_lap_tuyen_tinh(self):
         bai_toan.nhap_du_lieu(kiem_tra_dltt["a"])
-        result = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result[0], True, "Should be True")
+        result = bai_toan.kiem_tra_dltt()
+        self.assertEqual(result, True, "Should be True")
 
     def test_phu_thuoc_tuyen_tinh(self):
         bai_toan.nhap_du_lieu(kiem_tra_dltt["b"])
-        result_1 = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result_1[0], False, "Should be False")
+        result_1 = bai_toan.kiem_tra_dltt()
+        self.assertEqual(result_1, False, "Should be False")
 
         bai_toan.nhap_du_lieu(kiem_tra_dltt["c"])
-        result_2 = bai_toan.kiem_tra_thtt()
-        self.assertEqual(result_2[0], False, "Should be False")
+        result_2 = bai_toan.kiem_tra_dltt()
+        self.assertEqual(result_2, False, "Should be False")
 
 
 class TestKhongGianVector(unittest.TestCase):
@@ -65,12 +65,14 @@ class TestKhongGianVector(unittest.TestCase):
     def test_tim_theo_chieu(self):
         bai_toan.nhap_du_lieu(tim_co_so["a"])
         result = bai_toan.tim_co_so()
-        self.assertEqual(result[0], True, "Should be True")
+        solution = [[1, 2, 1, 1], [0, 0, 1, 2], [0, 0, 0, 1]]
+        self.assertEqual(result == solution, True, "Should be True")
 
     def test_tim_theo_hang(self):
         bai_toan.nhap_du_lieu(tim_co_so["b"])
         result = bai_toan.tim_co_so()
-        self.assertEqual(result[0], True, "Should be True")
+        solution = [[1, -2, -1, 3], [0, 0, -1, -6], [0, 0, 0, 1]]
+        self.assertEqual(result == solution, True, "Should be True")
 
 
 if __name__ == '__main__':
